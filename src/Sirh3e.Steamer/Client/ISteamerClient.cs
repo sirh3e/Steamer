ffd4049c02b9a7cfa15be4085dec09480fc75f5f
sirh3e <sirh3e@protonmail.com>
2021-01-28 21:06:21 +0100

    added SteamerClient

---
+++ src/Sirh3e.Steamer/Client/ISteamerClient.cs
@@ -0,0 +1,9 @@
+﻿using Sirh3e.Steamer.Auth;
+
+namespace Sirh3e.Steamer.Client
+{
+    public interface ISteamerClient
+    {
+        public ISteamerAuthProvider AuthProvider { get; }
+    }
+}
---
+++ src/Sirh3e.Steamer/Client/SteamerClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Sirh3e.Steamer.Auth;
+
+namespace Sirh3e.Steamer.Client
+{
+    public class SteamerClient : ISteamerClient
+    {
+        public SteamerClient(ISteamerAuthProvider authProvider)
+        {
+            AuthProvider = authProvider ?? throw new ArgumentNullException(nameof(authProvider));
+        }
+
+        public ISteamerAuthProvider AuthProvider { get; }
+    }
+}
