b214f6c07ddf634bd87e5f0403e6b4259bd9e4c4
sirh3e <sirh3e@protonmail.com>
2021-02-07 13:54:04 +0100

    changed PlayerBansRequest added SteamerRequest

--- src/Sirh3e.Steamer/Web/Builders/Interfaces/SteamUser/PlayerBans/Request/PlayerBansRequest.cs
+++ src/Sirh3e.Steamer/Web/Builders/Interfaces/SteamUser/PlayerBans/Request/PlayerBansRequest.cs
@@ -1,13 +1,22 @@
-﻿namespace Sirh3e.Steamer.Web.Builders.Interfaces.SteamUser.PlayerBans.Request
+﻿using System.Net.Http;
+using Sirh3e.Steamer.Core.Interface;
+using Sirh3e.Steamer.Core.Method;
+using Sirh3e.Steamer.Core.Parameter.Types;
+using Sirh3e.Steamer.Core.Parameters;
+using Sirh3e.Steamer.Core.Request;
+
+namespace Sirh3e.Steamer.Web.Builders.Interfaces.SteamUser.PlayerBans.Request
 {
-    /*
     public class PlayerBansRequest : SteamerRequest, IPlayerBansRequest
     {
         public ISteamerMethod Method { get; set; }
 
-        public PlayerBansRequest() : base(method)
+        public PlayerBansRequest(ISteamerInterface @interface) : base(new SteamerMethod(@interface,
+                                                            HttpMethod.Get, "GetPlayerBans", 1,
+                                                            new SteamerParameters(
+                                                                                  new SteamerStringParameter("key"),
+                                                                                  new SteamerStringParameter("steamids"))))
         {
         }
     }
-    */
 }
