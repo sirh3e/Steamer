c492dacad62cc3dab0f6e0fc1c55503c15c27beb
sirh3e <sirh3e@protonmail.com>
2021-02-27 17:58:03 +0100

    [ADDED] SteamerWebTradeOffersSummaryResponse*.cs

---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/TradeOffersSummary/Response/ISteamerWebTradeOffersSummaryResponse.cs
@@ -0,0 +1,13 @@
+﻿using Sirh3e.Steamer.Core.Response;
+using Sirh3e.Steamer.Web.Models;
+using Sirh3e.Steamer.Web.Models.EconService.TradeOffer;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffer.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffersSummary.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffersSummary.Response
+{
+    public interface ISteamerWebTradeOffersSummaryResponse : ISteamerResponse<ISteamerWebTradeOffersSummaryRequest,
+        SteamerWebResponseModel<SteamerWebTradeOffersSummaryModel>>
+    {
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/TradeOffersSummary/Response/SteamerWebTradeOffersSummaryResponse.cs
@@ -0,0 +1,14 @@
+﻿using Sirh3e.Rust.Option;
+using Sirh3e.Steamer.Web.Models;
+using Sirh3e.Steamer.Web.Models.EconService.TradeOffer;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffer.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffersSummary.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffersSummary.Response
+{
+    public class SteamerWebTradeOffersSummaryResponse : ISteamerWebTradeOffersSummaryResponse
+    {
+        public ISteamerWebTradeOffersSummaryRequest Request { get; init; }
+        public Option<SteamerWebResponseModel<SteamerWebTradeOffersSummaryModel>> Model { get; init; }
+    }
+}
