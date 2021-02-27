20149f6d589078be5adf12726fcaf84c4cb5f5aa
sirh3e <sirh3e@protonmail.com>
2021-02-27 21:03:44 +0100

    [ADDED] added {I,}SteamerWebSteamUserStatsInterfaceBuilder.cs

---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/SteamUserStats/ISteamerWebSteamUserStatsInterfaceBuilder.cs
@@ -0,0 +1,7 @@
+﻿namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats
+{
+    public interface ISteamerWebSteamUserStatsInterfaceBuilder
+    {
+        
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/SteamUserStats/SteamerWebSteamUserStatsInterfaceBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Sirh3e.Steamer.Core.Interface;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats
+{
+    public class SteamerWebSteamUserStatsInterfaceBuilder : SteamerInterface, ISteamerWebSteamUserStatsInterfaceBuilder
+    {
+        public SteamerWebSteamUserStatsInterfaceBuilder() : base("ISteamUserStats")
+        {
+        }
+    }
+}
