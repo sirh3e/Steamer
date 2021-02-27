24ed2980f54e32383b295957e0f85c3cca8d7c83
sirh3e <sirh3e@protonmail.com>
2021-02-27 20:38:46 +0100

    [ADDED] added CancelTradeOffer repsonse and model

---
+++ src/Sirh3e.Steamer/Web/Models/EconService/CancelTradeOffer/SteamerWebCancelTradeOfferModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.EconService.CancelTradeOffer
+{
+    public class SteamerWebCancelTradeOfferModel
+    {
+        [JsonPropertyName("response")]
+        public object Response { get; set; }
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/CancelTradeOffer/Response/ISteamerWebCancelTradeOfferResponse.cs
@@ -0,0 +1,12 @@
+﻿using Sirh3e.Steamer.Core.Response;
+using Sirh3e.Steamer.Web.Models.EconService.CancelTradeOffer;
+using Sirh3e.Steamer.Web.Models.EconService.DeclineTradeOffer;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.DeclineTradeOffer.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.CancelTradeOffer.Response
+{
+    public interface ISteamerWebCancelTradeOfferResponse : ISteamerResponse<ISteamerWebDeclineTradeOfferRequest,
+        SteamerWebCancelTradeOfferModel>
+    {
+    }
+}
