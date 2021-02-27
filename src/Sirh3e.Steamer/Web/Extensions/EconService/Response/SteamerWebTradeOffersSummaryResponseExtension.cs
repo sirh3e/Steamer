24e9c9fe6259be262646a70385359b621ddde7e0
sirh3e <sirh3e@protonmail.com>
2021-02-27 18:08:49 +0100

    [ADDED] SteamerWebTradeOffersSummaryResponseExtension with method RetryServiceExecute

--- src/Sirh3e.Steamer/Web/Extensions/EconService/Response/SteamerWebTradeOffersSummaryResponseExtension.cs
+++ src/Sirh3e.Steamer/Web/Extensions/EconService/Response/SteamerWebTradeOffersSummaryResponseExtension.cs
@@ -1,7 +1,19 @@
-﻿namespace Sirh3e.Steamer.Web.Extensions.EconService.Response
+﻿using System;
+using Sirh3e.Steamer.Web.Extensions.EconService.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffersSummary.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.EconService.Response
 {
-    public class SteamerWebTradeOffersSummaryResponseExtension
+    public static class SteamerWebTradeOffersSummaryResponseExtension
     {
-        
+        public static ISteamerWebTradeOffersSummaryResponse RetryServiceExecute(this ISteamerWebTradeOffersSummaryResponse response,
+            ISteamerWebService service)
+        {
+            _ = response ?? throw new ArgumentNullException(nameof(response));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return response.Request.ServiceExecute(service);
+        } 
     }
 }
