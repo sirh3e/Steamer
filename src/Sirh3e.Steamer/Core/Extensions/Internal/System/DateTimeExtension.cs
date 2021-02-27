8dfe741bccf73ea1687ef751bb8fa2916a52f51f
sirh3e <sirh3e@protonmail.com>
2021-02-27 12:41:55 +0100

    [MODIFIED] src/Sirh3e.Steamer/Core/Parameter/Types/SteamerDateTimeParameter.cs support for unixtimestamps

---
+++ src/Sirh3e.Steamer/Core/Extensions/Internal/System/DateTimeExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sirh3e.Steamer.Core.Extensions.Internal.System
+{
+    internal static class DateTimeExtension
+    {
+        public static uint ToUnixTimeStamp(this DateTime dateTime)
+        {
+            return (uint)dateTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+        }
+    }
+}
---
+++ src/Sirh3e.Steamer/Core/Parameter/Types/SteamerDateTimeParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Sirh3e.Steamer.Core.Extensions.Internal.System;
+
+namespace Sirh3e.Steamer.Core.Parameter.Types
+{
+    public class SteamerDateTimeParameter : SteamerParameter<DateTime>
+    {
+        public SteamerDateTimeParameter(string name, DateTime value, bool required = true) : base(name, value, required)
+        {
+        }
+
+        public override string GetValueFromQueryString()
+        {
+            return Value.ToUnixTimeStamp().ToString();
+        }
+    }
+}
