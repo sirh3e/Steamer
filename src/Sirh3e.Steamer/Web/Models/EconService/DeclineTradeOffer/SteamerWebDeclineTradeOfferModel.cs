507cbe5acf1ad2c525d983247a678fb947e4977b
sirh3e <sirh3e@protonmail.com>
2021-02-27 19:50:23 +0100

    [ADDED] src/Sirh3e.Steamer/Web/Models/EconService/DeclineTradeOffer/SteamerWebDeclineTradeOfferModel.cs

---
+++ src/Sirh3e.Steamer/Web/Models/EconService/DeclineTradeOffer/SteamerWebDeclineTradeOfferModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.EconService.DeclineTradeOffer
+{
+    public class SteamerWebDeclineTradeOfferModel
+    {
+        [JsonPropertyName("response")]
+        public object Response { get; set; } 
+    }
+}
--- src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/DeclineTradeOffer/Response/ISteamerWebDeclineTradeOfferResponse.cs
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/DeclineTradeOffer/Response/ISteamerWebDeclineTradeOfferResponse.cs
@@ -1,5 +1,6 @@
 ﻿using Sirh3e.Steamer.Core.Response;
 using Sirh3e.Steamer.Web.Models;
+using Sirh3e.Steamer.Web.Models.EconService.DeclineTradeOffer;
 using Sirh3e.Steamer.Web.Models.EconService.TradeHistory;
 using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeHistory.Request;
 
