1b1ffe8ed3ed32c4cd045bd73a6a722dfabecac9
sirh3e <sirh3e@protonmail.com>
2021-02-27 11:54:59 +0100

    [MODIFIED] src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/ISteamerWebEconServiceInterfaceBuilder.cs added ISteamerWebTradeOffferRequestBuilder.cs

--- src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/ISteamerWebEconServiceInterfaceBuilder.cs
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/ISteamerWebEconServiceInterfaceBuilder.cs
@@ -1,10 +1,12 @@
 ﻿using Sirh3e.Steamer.Core.Interface;
 using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeHistory.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffer.Request;
 
 namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService
 {
     public interface ISteamerWebEconServiceInterfaceBuilder : ISteamerInterface
     {
         public ISteamerWebTradeHistoryRequestBuilder TradeHistory { get; }
+        public ISteamerWebTradeOfferRequestBuilder TradeOffer { get;}
     }
 }
--- src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/SteamerWebEconServiceInterfaceBuilder.cs
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/SteamerWebEconServiceInterfaceBuilder.cs
@@ -1,12 +1,18 @@
 ﻿using Sirh3e.Steamer.Core.Interface;
 using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeHistory.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffer.Request;
 
 namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService
 {
     public class SteamerWebEconServiceInterfaceBuilder : SteamerInterface, ISteamerWebEconServiceInterfaceBuilder
     {
-        public SteamerWebEconServiceInterfaceBuilder() : base("IEconService") => TradeHistory = new SteamerWebTradeHistoryRequestBuilder(this);
+        public SteamerWebEconServiceInterfaceBuilder() : base("IEconService")
+        {
+            TradeHistory = new SteamerWebTradeHistoryRequestBuilder(this);
+            TradeOffer = new SteamerWebTradeOfferRequestBuilder(this);
+        }
 
         public ISteamerWebTradeHistoryRequestBuilder TradeHistory { get; }
+        public ISteamerWebTradeOfferRequestBuilder TradeOffer { get;}
     }
 }
