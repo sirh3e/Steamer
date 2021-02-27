e32eab6377996bf3c900fa68146b07957c70684f
sirh3e <sirh3e@protonmail.com>
2021-02-27 11:37:14 +0100

    [ADDED] src/Sirh3e.Steamer/Web/Models/EconService/TradeOffer/*.cs like models

---
+++ src/Sirh3e.Steamer/Web/Models/EconService/TradeOffer/SteamerWebTradeOfferModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.EconService.TradeOffer
+{
+    public class SteamerWebTradeOfferModel
+    {
+        [JsonPropertyName("offer")]
+        public SteamerWebOfferModel Offer { get; set; }
+    }
+}
