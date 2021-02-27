685e3b47eeecf7faa4e8617756f4577999760294
sirh3e <sirh3e@protonmail.com>
2021-02-27 19:35:31 +0100

    [ADDED] src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/DeclineTradeOffer/Request/*.cs files

---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/DeclineTradeOffer/Request/ISteamerWebDeclineTradeOfferRequestBuilder.cs
@@ -0,0 +1,10 @@
+﻿using Sirh3e.Steamer.Core.Builder;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.DeclineTradeOffer.Request.Request
+{
+    public interface ISteamerWebDeclineTradeOfferRequestBuilder : ISteamerRequestBuilder<ISteamerWebDeclineTradeOfferRequest>
+    {
+        public ISteamerWebDeclineTradeOfferRequestBuilder SetKey(string key);
+        public ISteamerWebDeclineTradeOfferRequestBuilder SetTradeOfferId(ulong tradeOfferId);
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/DeclineTradeOffer/Request/SteamerWebDeclineTradeOfferRequestBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Sirh3e.Steamer.Core.Builder;
+using Sirh3e.Steamer.Core.Interface;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.DeclineTradeOffer.Request.Request
+{
+    public class SteamerWebDeclineTradeOfferRequestBuilder : SteamerRequestBuilder<SteamerWebDeclineTradeOfferRequestBuilder,
+                                                                 ISteamerWebDeclineTradeOfferRequest>,
+                                                             ISteamerWebDeclineTradeOfferRequestBuilder
+    {
+        public SteamerWebDeclineTradeOfferRequestBuilder(ISteamerInterface @interface) : base(@interface) =>
+            Request = new SteamerWebDeclineTradeOfferRequest(Interface ??
+                                                             throw new ArgumentNullException(nameof(Interface)));
+
+        public override ISteamerWebDeclineTradeOfferRequest Build()
+        {
+            var request = Request;
+
+            Request = new SteamerWebDeclineTradeOfferRequest(Interface ??
+                                                             throw new ArgumentNullException(nameof(Interface)));
+
+            return request;
+        }
+
+        public ISteamerWebDeclineTradeOfferRequestBuilder SetKey(string key)
+            => SetValue("key", key ?? throw new ArgumentNullException(nameof(key)));
+
+        public ISteamerWebDeclineTradeOfferRequestBuilder SetTradeOfferId(ulong tradeOfferId)
+            => SetValue("tradeofferid", tradeOfferId);
+    }
+}
