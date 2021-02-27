5fc437e0acf857b07f2f81a87270c828c802c35c
sirh3e <sirh3e@protonmail.com>
2021-02-27 20:46:55 +0100

    [ADDED] added SteamerWebCancelTradeOfferRequestExtension.cs

---
+++ src/Sirh3e.Steamer/Web/Extensions/EconService/Request/SteamerWebCancelTradeOfferRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.CancelTradeOffer.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.CancelTradeOffer.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.EconService.Request
+{
+    public static class SteamerWebCancelTradeOfferRequestExtension
+    {
+        public static ISteamerWebCancelTradeOfferResponse ServiceExecute(this ISteamerWebCancelTradeOfferRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        }
+    }
+}
