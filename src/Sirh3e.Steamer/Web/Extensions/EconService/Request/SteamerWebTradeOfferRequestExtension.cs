dec75b303d7110e1ec8227fe41204300adfd7f5e
sirh3e <sirh3e@protonmail.com>
2021-02-27 16:07:41 +0100

    [ADDED] SteamerWebTradeOfferRequestExtension.cs method ServiceExecute

---
+++ src/Sirh3e.Steamer/Web/Extensions/EconService/Request/SteamerWebTradeOfferRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffer.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffer.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.EconService.Request
+{
+    public static class SteamerWebTradeOfferRequestExtension
+    {
+        public static ISteamerWebTradeOfferResponse ServiceExecute(this ISteamerWebTradeOfferRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        }
+    }
+}
