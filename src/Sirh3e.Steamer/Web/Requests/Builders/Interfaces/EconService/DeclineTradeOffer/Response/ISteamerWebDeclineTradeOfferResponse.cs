242cea9ea43a64e8b99762b517590a5ffb476dd5
sirh3e <sirh3e@protonmail.com>
2021-02-27 19:38:47 +0100

    [ADDED] Response/*.cs added

---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/DeclineTradeOffer/Response/ISteamerWebDeclineTradeOfferResponse.cs
@@ -0,0 +1,12 @@
+﻿using Sirh3e.Steamer.Core.Response;
+using Sirh3e.Steamer.Web.Models;
+using Sirh3e.Steamer.Web.Models.EconService.TradeHistory;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeHistory.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.DeclineTradeOffer.Response
+{
+    public interface ISteamerWebDeclineTradeOfferResponse : ISteamerResponse<ISteamerWebTradeHistoryRequest,
+        SteamerWebDeclineTradeOfferModel>
+    {
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/DeclineTradeOffer/Response/SteamerWebDeclineTradeOfferResponse.cs
@@ -0,0 +1,13 @@
+﻿using Sirh3e.Rust.Option;
+using Sirh3e.Steamer.Web.Models;
+using Sirh3e.Steamer.Web.Models.EconService.TradeHistory;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeHistory.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.DeclineTradeOffer.Response
+{
+    public class SteamerWebDeclineTradeOfferResponse : ISteamerWebDeclineTradeOfferResponse
+    {
+        public ISteamerWebTradeHistoryRequest Request { get; init; }
+        public Option<SteamerWebDeclineTradeOfferModel> Model { get; init; }
+    }
+}
