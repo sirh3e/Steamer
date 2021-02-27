b8cd7424c1760ba3f495d490c68182bd10076c8b
sirh3e <sirh3e@protonmail.com>
2021-02-27 20:03:20 +0100

    [ADDED] src/Sirh3e.Steamer/Web/Extensions/EconService/Response/SteamerWebDeclineTradeOfferSponseExtension.cs function RetryServiceExecute

---
+++ src/Sirh3e.Steamer/Web/Extensions/EconService/Response/SteamerWebDeclineTradeOfferResponseExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Extensions.EconService.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.DeclineTradeOffer.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.EconService.Response
+{
+    public static class SteamerWebDeclineTradeOfferResponseExtension
+    {
+        public static ISteamerWebDeclineTradeOfferResponse RetryServiceExecute(this ISteamerWebDeclineTradeOfferResponse response,
+            ISteamerWebService service)
+        {
+            _ = response ?? throw new ArgumentNullException(nameof(response));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return response.Request.ServiceExecute(service);
+        } 
+    }
+}
