db0595773a7408176ef47d7a8a5f301df963072e
sirh3e <sirh3e@protonmail.com>
2021-02-27 12:56:32 +0100

    [MODIFIED] src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/TradeOffer/Respone/ refactored

--- src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/TradeOffer/Response/SteamerWebTradeOfferResponse.cs
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/EconService/TradeOffer/Response/SteamerWebTradeOfferResponse.cs
@@ -1,9 +1,6 @@
 ﻿using Sirh3e.Rust.Option;
 using Sirh3e.Steamer.Web.Models;
-using Sirh3e.Steamer.Web.Models.EconService.TradeHistory;
 using Sirh3e.Steamer.Web.Models.EconService.TradeOffer;
-using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeHistory.Request;
-using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeHistory.Response;
 using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffer.Request;
 
 namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffer.Response
