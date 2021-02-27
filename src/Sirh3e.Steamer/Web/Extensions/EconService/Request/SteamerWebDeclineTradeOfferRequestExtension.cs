0e256c74ae547c8643d56e0cdf90b34bcaa1b9ba
sirh3e <sirh3e@protonmail.com>
2021-02-27 20:02:05 +0100

    [ADDED] src/Sirh3e.Steamer/Web/Extensions/EconService/Request/SteamerWebDeclineTradeOfferRequestExtension.cs function ServiceExecute

---
+++ src/Sirh3e.Steamer/Web/Extensions/EconService/Request/SteamerWebDeclineTradeOfferRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.DeclineTradeOffer.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.DeclineTradeOffer.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.EconService.Request
+{
+    public static class SteamerWebDeclineTradeOfferRequestExtension
+    {
+        public static ISteamerWebDeclineTradeOfferResponse ServiceExecute(this ISteamerWebDeclineTradeOfferRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        } 
+    }
+}
