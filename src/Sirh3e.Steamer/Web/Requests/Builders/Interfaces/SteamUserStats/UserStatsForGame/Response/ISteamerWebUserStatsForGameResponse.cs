24d0fe3980949558532ec40067be8a198016ffdf
sirh3e <sirh3e@protonmail.com>
2021-02-28 14:20:06 +0100

    [ADDED] Requests/Builders/Interfaces/SteamUserStats/UserStatsForGame/Response/

---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/SteamUserStats/UserStatsForGame/Response/ISteamerWebUserStatsForGameResponse.cs
@@ -0,0 +1,13 @@
+﻿using Sirh3e.Steamer.Core.Response;
+using Sirh3e.Steamer.Web.Models.EconService.CancelTradeOffer;
+using Sirh3e.Steamer.Web.Models.SteamUserStats.UserStatsForGame;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.CancelTradeOffer.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.UserStatsForGame.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.UserStatsForGame.Response
+{
+    public interface ISteamerWebUserStatsForGameResponse : ISteamerResponse<ISteamerWebUserStatsForGameRequest,
+        SteamerWebUserStatsForGameModel>
+    {
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/SteamUserStats/UserStatsForGame/Response/SteamerWebDeclineTradeOfferResponse.cs
@@ -0,0 +1,14 @@
+﻿using Sirh3e.Rust.Option;
+using Sirh3e.Steamer.Web.Models.EconService.CancelTradeOffer;
+using Sirh3e.Steamer.Web.Models.SteamUserStats.UserStatsForGame;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.CancelTradeOffer.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.UserStatsForGame.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.UserStatsForGame.Response
+{
+    public class SteamerWebUserStatsForGameResponse : ISteamerWebUserStatsForGameResponse
+    {
+        public ISteamerWebUserStatsForGameRequest Request { get; init; }
+        public Option<SteamerWebUserStatsForGameModel> Model { get; init; }
+    }
+}
