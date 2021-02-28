c69494e6fea41a54f1ac9ee261a45a082c8fbd03
sirh3e <sirh3e@protonmail.com>
2021-02-28 14:32:34 +0100

    [ADDED] Extensions/Interfaces/SteamUserStats/Request/SteamerWebUserStatsForGameRequestExtension.cs added function extension ServiceExecute

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamUserStats/Request/SteamerWebUserStatsForGameRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.UserStatsForGame.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.UserStatsForGame.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Request
+{
+    public static class SteamerWebUserStatsForGameRequestExtension
+    {
+        public static ISteamerWebUserStatsForGameResponse ServiceExecute(this ISteamerWebUserStatsForGameRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        }
+    }
+}
