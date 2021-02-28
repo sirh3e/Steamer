8683cd58f70e02a277612a1573099bc783a47df8
sirh3e <sirh3e@protonmail.com>
2021-02-28 14:29:47 +0100

    [MODIFIED] /Core/Clients/Web/SteamerWebClient.cs added Requests/Builders/Interfaces/SteamUserStats/SteamerWebSteamUserStatsInterfaceBuilder.cs

--- src/Sirh3e.Steamer/Core/Clients/Web/SteamerWebClient.cs
+++ src/Sirh3e.Steamer/Core/Clients/Web/SteamerWebClient.cs
@@ -3,6 +3,7 @@ using Sirh3e.Steamer.Core.Auth;
 using Sirh3e.Steamer.Core.Serializers.Providers;
 using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService;
 using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUser;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats;
 
 namespace Sirh3e.Steamer.Core.Clients.Web
 {
@@ -16,12 +17,14 @@ namespace Sirh3e.Steamer.Core.Clients.Web
             //Builders
             EconService = new SteamerWebEconServiceInterfaceBuilder();
             SteamUser = new SteamerWebUserInterfaceBuilder();
+            SteamUserStats = new SteamerWebSteamUserStatsInterfaceBuilder();
         }
 
         public ISteamerAuthProvider AuthProvider { get; }
         public ISteamerSerializerProvider SerializerProvider { get; }
         public ISteamerWebEconServiceInterfaceBuilder EconService { get; }
         public ISteamerWebUserInterfaceBuilder SteamUser { get; }
+        public ISteamerWebSteamUserStatsInterfaceBuilder SteamUserStats { get; }
 
         public class Builder
         {
