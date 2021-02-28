c14d5cd10faf1e334b74e0cfe04a7e1333256a02
sirh3e <sirh3e@protonmail.com>
2021-02-28 15:53:06 +0100

    [MODIFIED] Web/Extensions/Interfaces/SteamUserStats/Response/SteamerWebSchemaForGameResponseExtension.cs applied dotnet format

--- src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamUserStats/Response/SteamerWebSchemaForGameResponseExtension.cs
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamUserStats/Response/SteamerWebSchemaForGameResponseExtension.cs
@@ -14,6 +14,6 @@ namespace Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Response
             _ = service ?? throw new ArgumentNullException(nameof(service));
 
             return response.Request.ServiceExecute(service);
-        } 
+        }
     }
 }
