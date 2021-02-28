301a462b5b0754a8379ab0e1cf9049bda0254a6c
sirh3e <sirh3e@protonmail.com>
2021-02-28 21:47:42 +0100

    [ADDED] Web/Requests/Builders/Interfaces/PlayerService/ added SteamerWebPlayerServiceInterfaceBuilder.cs

---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/ISteamerWebPlayerServiceInterfaceBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Sirh3e.Steamer.Core.Interface;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService
+{
+    public interface ISteamerWebPlayerServiceInterfaceBuilder : ISteamerInterface
+    {
+
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/SteamerWebPlayerServiceInterfaceBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Sirh3e.Steamer.Core.Interface;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService
+{
+    public class SteamerWebPlayerServiceInterfaceBuilder : SteamerInterface, ISteamerWebPlayerServiceInterfaceBuilder
+    {
+        public SteamerWebPlayerServiceInterfaceBuilder() : base("IPlayerService")
+        {
+        }
+    }
+}
