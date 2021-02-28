655a33a63ef7de9c2a9c8980126fe2406687eb19
sirh3e <sirh3e@protonmail.com>
2021-02-28 21:01:33 +0100

    [ADDED] Extensions/Interfaces/SteamUserStats/Response/SteamerWebNumberOfCurrentPlayersResponseExtension.cs response extension

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamUserStats/Response/SteamerWebNumberOfCurrentPlayersResponseExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.NumberOfCurrentPlayers.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Response
+{
+    public static class SteamerWebNumberOfCurrentPlayersResponseExtension
+    {
+        public static ISteamerWebNumberOfCurrentPlayersResponse RetryServiceExecute(this ISteamerWebNumberOfCurrentPlayersResponse response,
+            ISteamerWebService service)
+        {
+            _ = response ?? throw new ArgumentNullException(nameof(response));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return response.Request.ServiceExecute(service);
+        }
+    }
+}
