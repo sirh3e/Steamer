5a8d05924bbae82b2ef7ca4c3445d885fb7349aa
sirh3e <sirh3e@protonmail.com>
2021-02-28 14:34:08 +0100

    [ADDED] Extensions/Interfaces/SteamUserStats/Response/SteamerWebUserStatsForGameResponseExtension.cs added function extension RetryServiceExecute

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamUserStats/Response/SteamerWebUserStatsForGameResponseExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.UserStatsForGame.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Response
+{
+    public static class SteamerWebUserStatsForGameResponseExtension
+    {
+        public static ISteamerWebUserStatsForGameResponse RetryServiceExecute(this ISteamerWebUserStatsForGameResponse response,
+                                                                                ISteamerWebService service)
+        {
+            _ = response ?? throw new ArgumentNullException(nameof(response));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return response.Request.ServiceExecute(service);
+        }
+    }
+}
