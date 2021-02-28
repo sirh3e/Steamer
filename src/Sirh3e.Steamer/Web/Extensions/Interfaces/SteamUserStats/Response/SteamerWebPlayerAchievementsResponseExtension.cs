35645816e76fa90db6e5de3fa0d18299b668f2ff
sirh3e <sirh3e@protonmail.com>
2021-02-28 20:32:24 +0100

    [ADDED] Web/Extensions/Interfaces/SteamUserStats/Response/SteamerWebPlayerAchievementsResponseExtension.cs added response extension

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamUserStats/Response/SteamerWebPlayerAchievementsResponseExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.PlayerAchievements.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Response
+{
+    public static class SteamerWebPlayerAchievementsResponseExtension
+    {
+        public static ISteamerWebPlayerAchievementsResponse RetryServiceExecute(this ISteamerWebPlayerAchievementsResponse response,
+            ISteamerWebService service)
+        {
+            _ = response ?? throw new ArgumentNullException(nameof(response));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return response.Request.ServiceExecute(service);
+        }
+    }
+}
