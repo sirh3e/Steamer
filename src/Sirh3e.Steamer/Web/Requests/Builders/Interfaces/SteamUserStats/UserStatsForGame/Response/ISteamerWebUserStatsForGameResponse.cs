e0bb01fd6033d3d08237d7345896eb70154cc066
sirh3e <sirh3e@protonmail.com>
2021-02-28 14:21:53 +0100

    [MODIFIED] Requests/Builders/Interfaces/SteamUserStats/UserStatsForGame/Response/*.cs removed unused usings

--- src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/SteamUserStats/UserStatsForGame/Response/ISteamerWebUserStatsForGameResponse.cs
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/SteamUserStats/UserStatsForGame/Response/ISteamerWebUserStatsForGameResponse.cs
@@ -1,7 +1,5 @@
 ﻿using Sirh3e.Steamer.Core.Response;
-using Sirh3e.Steamer.Web.Models.EconService.CancelTradeOffer;
 using Sirh3e.Steamer.Web.Models.SteamUserStats.UserStatsForGame;
-using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.CancelTradeOffer.Request;
 using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.UserStatsForGame.Request;
 
 namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.UserStatsForGame.Response
