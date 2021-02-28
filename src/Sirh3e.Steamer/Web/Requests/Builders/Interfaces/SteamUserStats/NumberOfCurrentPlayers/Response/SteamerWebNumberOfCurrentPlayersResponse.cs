4b8ecff5e7eaf1c8d7473c7dcadaed6586ff9fd4
sirh3e <sirh3e@protonmail.com>
2021-02-28 20:53:02 +0100

    [ADDED]  NumberOfCurrentPlayers/Response/

---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/SteamUserStats/NumberOfCurrentPlayers/Response/SteamerWebNumberOfCurrentPlayersResponse.cs
@@ -0,0 +1,12 @@
+﻿using Sirh3e.Rust.Option;
+using Sirh3e.Steamer.Web.Models.SteamUserStats.NumberOfCurrentPlayers;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.NumberOfCurrentPlayers.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.NumberOfCurrentPlayers.Response
+{
+    public class SteamerWebNumberOfCurrentPlayersResponse : ISteamerWebNumberOfCurrentPlayersResponse
+    {
+        public ISteamerWebNumberOfCurrentPlayersRequest Request { get; init; }
+        public Option<SteamerWebNumberOfCurrentPlayersModel> Model { get; init; }
+    }
+}
