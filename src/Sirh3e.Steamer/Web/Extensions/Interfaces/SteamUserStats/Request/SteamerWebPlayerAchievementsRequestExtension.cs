8ad2be5d683b25b355efe317bd0367d24f33dcfe
sirh3e <sirh3e@protonmail.com>
2021-02-28 20:31:04 +0100

    [ADDED] Web/Extensions/Interfaces/SteamUserStats/Request/SteamerWebPlayerAchievementsRequestExtension.cs added request extension

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamUserStats/Request/SteamerWebPlayerAchievementsRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.PlayerAchievements.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.PlayerAchievements.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Request
+{
+    public static class SteamerWebPlayerAchievementsRequestExtension
+    {
+        public static ISteamerWebPlayerAchievementsResponse ServiceExecute(this ISteamerWebPlayerAchievementsRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        }
+    }
+}
