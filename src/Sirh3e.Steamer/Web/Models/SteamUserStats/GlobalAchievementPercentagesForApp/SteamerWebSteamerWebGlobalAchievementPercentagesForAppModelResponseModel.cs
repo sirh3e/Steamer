9e8c1c334ab67c83faba0c24ba3c2135ac6e4dc0
sirh3e <sirh3e@protonmail.com>
2021-02-28 21:42:16 +0100

    [ADDED] Web/Models/SteamUserStats/GlobalAchievementPercentagesForApp/SteamerWebSteamerWebGlobalAchievementPercentagesForAppModelResponseModel.cs added

---
+++ src/Sirh3e.Steamer/Web/Models/SteamUserStats/GlobalAchievementPercentagesForApp/SteamerWebSteamerWebGlobalAchievementPercentagesForAppModelResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.SteamUserStats.GlobalAchievementPercentagesForApp
+{
+    public class SteamerWebSteamerWebGlobalAchievementPercentagesForAppModelResponseModel
+    {
+        [JsonPropertyName("achievementpercentages")]
+        public SteamerWebGlobalAchievementPercentagesForAppModel AchievementPercentages { get; set; }
+    }
+}
