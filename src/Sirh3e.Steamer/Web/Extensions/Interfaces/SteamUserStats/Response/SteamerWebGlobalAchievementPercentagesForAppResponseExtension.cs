c9d2a3be53ba335c614f8333bf5d7f7ebba3a655
sirh3e <sirh3e@protonmail.com>
2021-02-28 21:31:08 +0100

    [ADDED] Web/Extensions/Interfaces/SteamUserStats/Response/SteamerWebGlobalAchievementPercentagesForAppRequestExtension.cs response extension

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamUserStats/Response/SteamerWebGlobalAchievementPercentagesForAppResponseExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.GlobalAchievementPercentagesForApp.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Response
+{
+    public static class SteamerWebGlobalAchievementPercentagesForAppResponseExtension
+    {
+        public static ISteamerWebGlobalAchievementPercentagesForAppResponse RetryServiceExecute(this ISteamerWebGlobalAchievementPercentagesForAppResponse response,
+            ISteamerWebService service)
+        {
+            _ = response ?? throw new ArgumentNullException(nameof(response));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return response.Request.ServiceExecute(service);
+        }
+    }
+}
