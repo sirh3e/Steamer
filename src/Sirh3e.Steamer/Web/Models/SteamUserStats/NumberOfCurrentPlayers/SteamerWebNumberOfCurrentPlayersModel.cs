9cedd64ba64cf7d38fa3d8328586689e9733b0bb
sirh3e <sirh3e@protonmail.com>
2021-02-28 20:41:50 +0100

    [ADDED] Web/Models/SteamUserStats/NumberOfCurrentPlayers/SteamerWebNumberOfCurrentPlayersModel.cs added model

---
+++ src/Sirh3e.Steamer/Web/Models/SteamUserStats/NumberOfCurrentPlayers/SteamerWebNumberOfCurrentPlayersModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.SteamUserStats.NumberOfCurrentPlayers
+{
+    public class SteamerWebNumberOfCurrentPlayersModel
+    {
+        [JsonPropertyName("player_count")]
+        public ulong PlayerCount { get; set; }
+
+        [JsonPropertyName("result")]
+        public ulong Result { get; set; }
+    }
+}
