4a6fca044042c228270cce7520d49284ad31728e
sirh3e <sirh3e@protonmail.com>
2021-02-28 15:51:30 +0100

    [ADDED] Web/Extensions/Interfaces/SteamUserStats/Request/SteamerWebSchemaForGameRequestExtension.cs added

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamUserStats/Request/SteamerWebSchemaForGameRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.SchemaForGame.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamUserStats.SchemaForGame.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Request
+{
+    public static class SteamerWebSchemaForGameRequestExtension
+    {
+        public static ISteamerWebSchemaForGameResponse ServiceExecute(this ISteamerWebSchemaForGameRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        }
+    }
+}
