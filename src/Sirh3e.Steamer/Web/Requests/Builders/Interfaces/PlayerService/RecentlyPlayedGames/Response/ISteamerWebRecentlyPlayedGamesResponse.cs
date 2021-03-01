9d07ddbc56f2c44e032e401cd51ee69142e7c963
sirh3e <sirh3e@protonmail.com>
2021-03-01 14:12:19 +0100

    [MODIFIED] Web/Requests/Builders/Interfaces/SteamUserStats/GlobalAchievementPercentagesForApp/ repsonses

--- src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/RecentlyPlayedGames/Response/ISteamerWebRecentlyPlayedGamesResponse.cs
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/RecentlyPlayedGames/Response/ISteamerWebRecentlyPlayedGamesResponse.cs
@@ -1,12 +1,12 @@
 ﻿using Sirh3e.Steamer.Core.Response;
 using Sirh3e.Steamer.Web.Models;
-using Sirh3e.Steamer.Web.Models.EconService.TradeOffersSummary;
-using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffersSummary.Request;
+using Sirh3e.Steamer.Web.Models.PlayerService.RecentlyPlayedGames;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.RecentlyPlayedGames.Request;
 
-namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffersSummary.Response
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.RecentlyPlayedGames.Response
 {
-    public interface ISteamerWebTradeOffersSummaryResponse : ISteamerResponse<ISteamerWebTradeOffersSummaryRequest,
-        SteamerWebResponseModel<SteamerWebTradeOffersSummaryModel>>
+    public interface ISteamerWebRecentlyPlayedGamesResponse : ISteamerResponse<ISteamerWebRecentlyPlayedGamesRequest,
+        SteamerWebResponseModel<SteamerWebRecentlyPlayedGamesModel>>
     {
     }
 }
--- src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/RecentlyPlayedGames/Response/SteamerWebRecentlyPlayedGamesResponse.cs
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/RecentlyPlayedGames/Response/SteamerWebRecentlyPlayedGamesResponse.cs
@@ -1,13 +1,13 @@
 ﻿using Sirh3e.Rust.Option;
 using Sirh3e.Steamer.Web.Models;
-using Sirh3e.Steamer.Web.Models.EconService.TradeOffersSummary;
-using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffersSummary.Request;
+using Sirh3e.Steamer.Web.Models.PlayerService.RecentlyPlayedGames;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.RecentlyPlayedGames.Request;
 
 namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.RecentlyPlayedGames.Response
 {
     public class SteamerWebRecentlyPlayedGamesResponse : ISteamerWebRecentlyPlayedGamesResponse
     {
-        public ISteamerWebTradeOffersSummaryRequest Request { get; init; }
-        public Option<SteamerWebResponseModel<SteamerWebTradeOffersSummaryModel>> Model { get; init; }
+        public ISteamerWebRecentlyPlayedGamesRequest Request { get; init; }
+        public Option<SteamerWebResponseModel<SteamerWebRecentlyPlayedGamesModel>> Model { get; init; }
     }
 }
