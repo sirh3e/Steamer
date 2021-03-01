bfe9aed643a9fd2ad8fcd1689112387100eed184
sirh3e <sirh3e@protonmail.com>
2021-03-01 15:53:47 +0100

    [ADDED] Web/Extensions/Interfaces/PlayerService/Request/SteamerWebBadgesRequestExtension.cs request extension

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/PlayerService/Request/SteamerWebBadgesRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.Badges.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.Badges.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.PlayerService.Request
+{
+    public static class SteamerWebBadgesRequestExtension
+    {
+        public static ISteamerWebBadgesResponse ServiceExecute(this ISteamerWebBadgesRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        }
+    }
+}
