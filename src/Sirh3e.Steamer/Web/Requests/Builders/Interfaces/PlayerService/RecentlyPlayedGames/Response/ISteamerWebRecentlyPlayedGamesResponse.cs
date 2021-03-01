10df1ec5257e972d5894649fc9fbb18fdd968653
sirh3e <sirh3e@protonmail.com>
2021-03-01 14:11:27 +0100

    [MODIFIED] removed unused usings

---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/RecentlyPlayedGames/Response/ISteamerWebRecentlyPlayedGamesResponse.cs
@@ -0,0 +1,12 @@
+﻿using Sirh3e.Steamer.Core.Response;
+using Sirh3e.Steamer.Web.Models;
+using Sirh3e.Steamer.Web.Models.EconService.TradeOffersSummary;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffersSummary.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffersSummary.Response
+{
+    public interface ISteamerWebTradeOffersSummaryResponse : ISteamerResponse<ISteamerWebTradeOffersSummaryRequest,
+        SteamerWebResponseModel<SteamerWebTradeOffersSummaryModel>>
+    {
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/RecentlyPlayedGames/Response/SteamerWebRecentlyPlayedGamesResponse.cs
@@ -0,0 +1,13 @@
+﻿using Sirh3e.Rust.Option;
+using Sirh3e.Steamer.Web.Models;
+using Sirh3e.Steamer.Web.Models.EconService.TradeOffersSummary;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.EconService.TradeOffersSummary.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.RecentlyPlayedGames.Response
+{
+    public class SteamerWebRecentlyPlayedGamesResponse : ISteamerWebRecentlyPlayedGamesResponse
+    {
+        public ISteamerWebTradeOffersSummaryRequest Request { get; init; }
+        public Option<SteamerWebResponseModel<SteamerWebTradeOffersSummaryModel>> Model { get; init; }
+    }
+}
