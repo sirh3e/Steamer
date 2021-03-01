0b14bbb1f1f7e96f1f5fe81c456359c5bc45ebef
sirh3e <sirh3e@protonmail.com>
2021-03-01 14:40:41 +0100

    [ADDED] Web/Models/PlayerService/OwnedGames/ repsonse models

---
+++ src/Sirh3e.Steamer/Web/Models/PlayerService/OwnedGames/SteamerWebGameModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.PlayerService.OwnedGames
+{
+    public class SteamerWebGameModel
+    {
+        [JsonPropertyName("appid")]
+        public ulong AppId { get; set; }
+
+        [JsonPropertyName("playtime_forever")]
+        public ulong PlaytimeForever { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("img_icon_url")]
+        public Uri ImgIconUrl { get; set; }
+
+        [JsonPropertyName("img_logo_url")]
+        public Uri ImgLogoUrl { get; set; }
+
+        [JsonPropertyName("has_community_visible_stats")]
+        public bool? HasCommunityVisibleStats { get; set; }
+
+        [JsonPropertyName("playtime_2weeks")]
+        public ulong? Playtime2Weeks { get; set; }
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Models/PlayerService/OwnedGames/SteamerWebOwnedGamesModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.PlayerService.OwnedGames
+{
+    public class SteamerWebOwnedGamesModel
+    {
+        [JsonPropertyName("game_count")]
+        public ulong GameCount { get; set; }
+
+        [JsonPropertyName("games")]
+        public List<SteamerWebGameModel> Games { get; set; } = new();
+    }
+}
