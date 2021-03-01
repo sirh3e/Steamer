b16f0fbc1d6b8f1b33d739087eb7f15774c1833b
sirh3e <sirh3e@protonmail.com>
2021-03-01 14:07:49 +0100

    [ADDED] RecentlyPlayedGames models

---
+++ src/Sirh3e.Steamer/Web/Models/PlayerService/RecentlyPlayedGames/SteamerWebGameModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.PlayerService.RecentlyPlayedGames
+{
+    public class SteamerWebGameModel
+    {
+        [JsonPropertyName("appid")]
+        public long Appid { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("playtime_2weeks")]
+        public ulong Playtime2Weeks { get; set; }
+
+        [JsonPropertyName("playtime_forever")]
+        public ulong PlaytimeForever { get; set; }
+
+        [JsonPropertyName("img_icon_url")]
+        public Uri ImgIconUrl { get; set; }
+
+        [JsonPropertyName("img_logo_url")]
+        public Uri ImgLogoUrl { get; set; }
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Models/PlayerService/RecentlyPlayedGames/SteamerWebRecentlyPlayedGamesModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.PlayerService.RecentlyPlayedGames
+{
+    public class SteamerWebRecentlyPlayedGamesModel
+    {
+        [JsonPropertyName("total_count")]
+        public ulong TotalCount { get; set; }
+
+        [JsonPropertyName("games")]
+        public List<SteamerWebGameModel> Games { get; set; } = new();
+    }
+}
