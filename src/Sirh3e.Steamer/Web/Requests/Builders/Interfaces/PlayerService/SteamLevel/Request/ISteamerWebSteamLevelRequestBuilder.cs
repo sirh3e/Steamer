53fe33e2cbf50382f9b168a9917bf12931b9b8f1
sirh3e <sirh3e@protonmail.com>
2021-03-01 15:00:32 +0100

    [ADDED] Web/Requests/Builders/Interfaces/PlayerService/SteamLevel/Request/ added *SteamLevelRequest*.cs

---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/SteamLevel/Request/ISteamerWebSteamLevelRequestBuilder.cs
@@ -0,0 +1,10 @@
+﻿using Sirh3e.Steamer.Core.Builder;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.SteamLevel.Request
+{
+    public interface ISteamerWebSteamLevelRequestBuilder : ISteamerRequestBuilder<ISteamerWebSteamLevelRequest>
+    {
+        public ISteamerWebSteamLevelRequestBuilder SetKey(string key);
+        public ISteamerWebSteamLevelRequestBuilder SetSteamId(ulong steamId);
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/SteamLevel/Request/SteamerWebSteamLevelRequestBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Sirh3e.Steamer.Core.Builder;
+using Sirh3e.Steamer.Core.Interface;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.SteamLevel.Request
+{
+    public class SteamerWebSteamLevelRequestBuilder
+        : SteamerRequestBuilder<SteamerWebSteamLevelRequestBuilder, ISteamerWebSteamLevelRequest>,
+            ISteamerWebSteamLevelRequestBuilder
+    {
+        public SteamerWebSteamLevelRequestBuilder(ISteamerInterface @interface) : base(@interface) =>
+            Request = new SteamerWebSteamLevelRequest(Interface ??
+                                                             throw new ArgumentNullException(nameof(Interface)));
+
+        public override ISteamerWebSteamLevelRequest Build()
+        {
+            var request = Request;
+
+            Request = new SteamerWebSteamLevelRequest(Interface ??
+                                                             throw new ArgumentNullException(nameof(Interface)));
+
+            return request;
+        }
+
+        public ISteamerWebSteamLevelRequestBuilder SetKey(string key)
+            => SetValue("key", key ?? throw new ArgumentNullException(nameof(key)));
+
+        public ISteamerWebSteamLevelRequestBuilder SetSteamId(ulong steamId)
+            => SetValue("steamid", steamId);
+    }
+}
