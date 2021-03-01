72660dd51855313770204fccde45fda31373acd1
sirh3e <sirh3e@protonmail.com>
2021-03-01 16:36:35 +0100

    [ADDED] SteamerWebCommunityBadgeProgressRequestExtension.cs request extension

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/PlayerService/Request/SteamerWebCommunityBadgeProgressRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.CommunityBadgeProgress.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.CommunityBadgeProgress.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.PlayerService.Request
+{
+    public static class SteamerWebCommunityBadgeProgressRequestExtension
+    {
+        public static ISteamerWebCommunityBadgeProgressResponse ServiceExecute(this ISteamerWebCommunityBadgeProgressRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        }
+    }
+}
