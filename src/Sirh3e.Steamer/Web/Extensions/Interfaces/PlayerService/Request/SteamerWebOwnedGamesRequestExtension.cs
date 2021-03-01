36ecd0c5468ada286b2aa9bbc46aac1c6c0ab007
sirh3e <sirh3e@protonmail.com>
2021-03-01 14:51:58 +0100

    [ADDED] Web/Extensions/Interfaces/PlayerService/Request/SteamerWebOwnedGamesRequestExtension.cs added request extension

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/PlayerService/Request/SteamerWebOwnedGamesRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.OwnedGames.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.OwnedGames.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.PlayerService.Request
+{
+    public static class SteamerWebOwnedGamesRequestExtension
+    {
+        public static ISteamerWebOwnedGamesResponse ServiceExecute(this ISteamerWebOwnedGamesRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        }
+    }
+}
