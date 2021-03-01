f41145dcf83d2ae58e997b6b3ca85d008424dda0
sirh3e <sirh3e@protonmail.com>
2021-03-01 15:02:06 +0100

    [ADDED]  Web/Models/PlayerService/SteamLevel/SteamerWebSteamLevelModel.cs added SteamerWebSteamLevelModel.cs model

---
+++ src/Sirh3e.Steamer/Web/Models/PlayerService/SteamLevel/SteamerWebSteamLevelModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.PlayerService.SteamLevel
+{
+    public class SteamerWebSteamLevelModel
+    {
+        [JsonPropertyName("player_level")]
+        public ulong PlayerLevel { get; set; }
+    }
+}
