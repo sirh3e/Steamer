c69dcdd360a38d74f00e747c33f410b258c2faf4
sirh3e <sirh3e@protonmail.com>
2021-03-01 16:13:29 +0100

    [ADDED] Web/Models/PlayerService/CommunityBadgeProgress/ models

---
+++ src/Sirh3e.Steamer/Web/Models/PlayerService/CommunityBadgeProgress/SteamerWebCommunityBadgeProgressModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.PlayerService.CommunityBadgeProgress
+{
+    public class SteamerWebCommunityBadgeProgressModel
+    {
+        [JsonPropertyName("quests")]
+        public List<SteamerWebQuestModel>? Quests { get; set; }
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Models/PlayerService/CommunityBadgeProgress/SteamerWebQuestModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.PlayerService.CommunityBadgeProgress
+{
+    public class SteamerWebQuestModel
+    {
+        [JsonPropertyName("questid")]
+        public ulong QuestId { get; set; }
+
+        [JsonPropertyName("completed")]
+        public bool Completed { get; set; }
+    }
+}
