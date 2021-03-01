e5cdb2229c691f423fdc3449fbe79cdf04b4b9a0
sirh3e <sirh3e@protonmail.com>
2021-03-01 14:22:50 +0100

    [ADDED] Web/Extensions/Interfaces/PlayerService/Request/SteamerWebRecentlyPlayedGamesRequestExtension.cs added request extension

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/PlayerService/Request/SteamerWebRecentlyPlayedGamesRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.RecentlyPlayedGames.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.RecentlyPlayedGames.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.PlayerService.Request
+{
+    public static class SteamerWebRecentlyPlayedGamesRequestExtension
+    {
+        public static ISteamerWebRecentlyPlayedGamesResponse ServiceExecute(this ISteamerWebRecentlyPlayedGamesRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        }
+    }
+}
