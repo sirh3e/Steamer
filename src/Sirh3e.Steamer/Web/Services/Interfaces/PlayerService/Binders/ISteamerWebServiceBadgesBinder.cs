db3b220fc76c31914412351702b345f53647ce17
sirh3e <sirh3e@protonmail.com>
2021-03-01 15:51:06 +0100

    [ADDED] Web/Services/Interfaces/PlayerService/Binders/ISteamerWebServiceBadgesBinder.cs added BadgesBinder to the project

---
+++ src/Sirh3e.Steamer/Web/Services/Interfaces/PlayerService/Binders/ISteamerWebServiceBadgesBinder.cs
@@ -0,0 +1,11 @@
+﻿using Sirh3e.Steamer.Core.Service;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.Badges.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.Badges.Response;
+
+namespace Sirh3e.Steamer.Web.Services.PlayerService.Binders
+{
+    public interface ISteamerWebServiceBadgesBinder
+        : ISteamerServiceBinder<ISteamerWebBadgesRequest, ISteamerWebBadgesResponse>
+    {
+    }
+}
