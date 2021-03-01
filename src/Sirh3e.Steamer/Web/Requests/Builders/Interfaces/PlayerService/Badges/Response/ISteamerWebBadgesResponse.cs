28e6191811b6daf7f34f782aa8fb5e2e933c12ed
sirh3e <sirh3e@protonmail.com>
2021-03-01 15:46:35 +0100

    [ADDED] Web/Requests/Builders/Interfaces/PlayerService/Badges/Response/ added repsonse

---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/Badges/Response/ISteamerWebBadgesResponse.cs
@@ -0,0 +1,12 @@
+﻿using Sirh3e.Steamer.Core.Response;
+using Sirh3e.Steamer.Web.Models;
+using Sirh3e.Steamer.Web.Models.PlayerService.Badges;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.Badges.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.Badges.Response
+{
+    public interface ISteamerWebBadgesResponse : ISteamerResponse<ISteamerWebBadgesRequest,
+        SteamerWebResponseModel<SteamerWebBadgesModel>>
+    {
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/PlayerService/Badges/Response/SteamerWebBadgesResponse.cs
@@ -0,0 +1,13 @@
+﻿using Sirh3e.Rust.Option;
+using Sirh3e.Steamer.Web.Models;
+using Sirh3e.Steamer.Web.Models.PlayerService.Badges;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.Badges.Request;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.Badges.Response
+{
+    public class SteamerWebBadgesResponse : ISteamerWebBadgesResponse
+    {
+        public ISteamerWebBadgesRequest Request { get; init; }
+        public Option<SteamerWebResponseModel<SteamerWebBadgesModel>> Model { get; init; }
+    }
+}
