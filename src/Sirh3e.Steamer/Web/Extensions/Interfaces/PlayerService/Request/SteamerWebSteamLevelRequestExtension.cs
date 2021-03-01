da49d197867710d1c25c42152016206e74ada993
sirh3e <sirh3e@protonmail.com>
2021-03-01 15:16:43 +0100

    [ADDED] Web/Extensions/Interfaces/PlayerService/Request/SteamerWebSteamLevelRequestExtension.cs added request extension

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/PlayerService/Request/SteamerWebSteamLevelRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.SteamLevel.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.PlayerService.SteamLevel.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.PlayerService.Request
+{
+    public static class SteamerWebSteamLevelRequestExtension
+    {
+        public static ISteamerWebSteamLevelResponse ServiceExecute(this ISteamerWebSteamLevelRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        }
+    }
+}
