2b249edd1cb8924d5a63ceb2705f47dee8a57074
sirh3e <sirh3e@protonmail.com>
2021-03-01 15:42:52 +0100

    [ADDED] Web/Models/PlayerService/Badges/ added response model

---
+++ src/Sirh3e.Steamer/Web/Models/PlayerService/Badges/SteamerWebBadgeModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+using Sirh3e.Steamer.Core.Serializers.Json.Converters;
+
+namespace Sirh3e.Steamer.Web.Models.PlayerService.Badges
+{
+    public class SteamerWebBadgeModel
+    {
+        [JsonPropertyName("badgeid")]
+        public int BadgeId { get; set; }
+
+        [JsonPropertyName("level")]
+        public ulong Level { get; set; }
+
+        [JsonPropertyName("completion_time")]
+        public ulong CompletionTime { get; set; }
+
+        [JsonPropertyName("xp")]
+        public ulong Xp { get; set; }
+
+        [JsonPropertyName("scarcity")]
+        public ulong Scarcity { get; set; }
+
+        [JsonPropertyName("appid")]
+        public uint? Appid { get; set; }
+
+        [JsonPropertyName("communityitemid")]
+        [JsonConverter(typeof(SteamerSystemTextJsonConverterStringToUlong))]
+        public ulong? CommunityItemId { get; set; }
+
+        [JsonPropertyName("border_color")]
+        public ulong? BorderColor { get; set; }
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Models/PlayerService/Badges/SteamerWebBadgesModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Sirh3e.Steamer.Web.Models.PlayerService.Badges
+{
+    public class SteamerWebBadgesModel
+    {
+        [JsonPropertyName("badges")]
+        public List<SteamerWebBadgeModel> Badges { get; set; } = new();
+
+        [JsonPropertyName("player_xp")]
+        public ulong PlayerXp { get; set; }
+
+        [JsonPropertyName("player_level")]
+        public ulong PlayerLevel { get; set; }
+
+        [JsonPropertyName("player_xp_needed_to_level_up")]
+        public ulong PlayerXpNeededToLevelUp { get; set; }
+
+        [JsonPropertyName("player_xp_needed_current_level")]
+        public ulong PlayerXpNeededCurrentLevel { get; set; }
+    }
+}
