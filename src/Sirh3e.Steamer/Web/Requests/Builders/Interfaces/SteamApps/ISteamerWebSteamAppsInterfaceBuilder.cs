702cea3d889be34efa2d6d9ff17243d89e1543c1
sirh3e <sirh3e@protonmail.com>
2021-03-01 16:46:16 +0100

    [ADDED] Web/Requests/Builders/Interfaces/SteamApps/ISteamerWebSteamAppsInterfaceBuilder.cs added InterfaceBuilder.cs

---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/SteamApps/ISteamerWebSteamAppsInterfaceBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Sirh3e.Steamer.Core.Interface;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamApps
+{
+    public interface ISteamerWebSteamAppsInterfaceBuilder : ISteamerInterface
+    {
+    }
+}
---
+++ src/Sirh3e.Steamer/Web/Requests/Builders/Interfaces/SteamApps/SteamerWebSteamAppsInterfaceBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Sirh3e.Steamer.Core.Interface;
+
+namespace Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamApps
+{
+    public class SteamerWebSteamAppsInterfaceBuilder : SteamerInterface, ISteamerWebSteamAppsInterfaceBuilder
+    {
+        public SteamerWebSteamAppsInterfaceBuilder() : base("ISteamApps")
+        {
+        }
+    }
+}
