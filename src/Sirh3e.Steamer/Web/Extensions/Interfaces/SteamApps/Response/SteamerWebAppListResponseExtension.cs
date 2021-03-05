40796eaed263d1904250be8d2f0d218c3213ce0b
sirh3e <sirh3e@protonmail.com>
2021-03-05 17:22:22 +0100

    [ADDED] src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamApps/Repsonse added reponse extension

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamApps/Response/SteamerWebAppListResponseExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Extensions.Interfaces.SteamApps.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamApps.AppList.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.SteamApps.Response
+{
+    public static class SteamerWebAppListResponseExtension
+    {
+        public static ISteamerWebAppListResponse RetryServiceExecute(this ISteamerWebAppListResponse response,
+            ISteamerWebService service)
+        {
+            _ = response ?? throw new ArgumentNullException(nameof(response));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return response.Request.ServiceExecute(service);
+        }
+    }
+}
