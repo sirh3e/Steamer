eb1ca9d1812b57e0d691ab7d19a3126428dfe605
sirh3e <sirh3e@protonmail.com>
2021-03-05 17:21:06 +0100

    [ADDED] src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamApps/Request/SteamerWebAppListRequestExtension.cs request extension

---
+++ src/Sirh3e.Steamer/Web/Extensions/Interfaces/SteamApps/Request/SteamerWebAppListRequestExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamApps.AppList.Request;
+using Sirh3e.Steamer.Web.Requests.Builders.Interfaces.SteamApps.AppList.Response;
+using Sirh3e.Steamer.Web.Services;
+
+namespace Sirh3e.Steamer.Web.Extensions.Interfaces.SteamApps.Request
+{
+    public static class SteamerWebAppListRequestExtension
+    {
+        public static ISteamerWebAppListResponse ServiceExecute(this ISteamerWebAppListRequest request,
+            ISteamerWebService service)
+        {
+            _ = request ?? throw new ArgumentNullException(nameof(request));
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            return service.Execute(request);
+        }
+    }
+}
