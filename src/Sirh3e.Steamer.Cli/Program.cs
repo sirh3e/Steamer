29320cdf56dc49e4b336693d068b0503c0a0a6f6
sirh3e <sirh3e@protonmail.com>
2021-03-26 18:09:54 +0100

    [MODIFIED] src/Sirh3e.Steamer.Cli

--- src/Sirh3e.Steamer.Cli/Program.cs
+++ src/Sirh3e.Steamer.Cli/Program.cs
@@ -1,41 +1,27 @@
 ﻿using System;
-using System.Net.Http;
-using System.Text.Json;
-using Sirh3e.Steamer.Core.Auth;
-using Sirh3e.Steamer.Core.Clients.Web;
-using Sirh3e.Steamer.Core.Net.Http.Clients.Providers;
-using Sirh3e.Steamer.Core.Serializers.Json;
-using Sirh3e.Steamer.Core.Serializers.Providers;
-using Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUser.Request;
-using Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUserStats.Request;
-using Sirh3e.Steamer.Web.Extensions.Rust;
-using Sirh3e.Steamer.Web.Services;
+using System.Threading.Tasks;
+using Sirh3e.Steamer.Web;
 
 namespace Sirh3e.Steamer.Cli
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
             var apiKey = "D307045B67D6513FFACE19257331AF5A";
-            var client = new SteamerWebClient.Builder()
-                .SetAuthProvider(new SteamerAuthProvider(apiKey))
-                .SetSerializerProvider(new SteamerSerializerProvider.Builder()
-                                           .SetSerializer(new SteamerSystemTextJsonSerializer(new
-                                                                                                  SteamerSystemTextJsonSerializerOptionsProvider(new
-                                                                                                      JsonSerializerOptions())))
-                                           .Build())
+            var (client, service) = SteamerWebFactory.CreateByApiKey(apiKey);
+
+            var request = client
+                .SteamApps
+                .AppList
                 .Build();
 
-            var httpClientProvider = new SteamerHttpClientProvider(new HttpClient());
-            var service = new SteamerWebService(client, httpClientProvider);
+            var response = service.Execute(request);
 
-            var response = client.SteamUserStats.NumberOfCurrentPlayers.SetAppId(730).Build().ServiceExecute(service);
             response.Model.Match(model =>
             {
-
+                model.AppList.Apps.ForEach(a => Console.WriteLine($"{a.AppId} {a.Name}"));
             }, () => { });
-
         }
     }
 }
