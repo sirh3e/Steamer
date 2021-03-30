e2d7352bd5f47a24518138d3c7db6899e56f0c6d
sirh3e <sirh3e@protonmail.com>
2021-03-30 15:25:33 +0200

    [MODIFIED] changed types

--- src/Sirh3e.Steamer/Web/Models/SteamUser/FriendList/SteamerWebFriendModel.cs
+++ src/Sirh3e.Steamer/Web/Models/SteamUser/FriendList/SteamerWebFriendModel.cs
@@ -1,16 +1,18 @@
 ﻿using System.Text.Json.Serialization;
+using Sirh3e.Steamer.Core.Serializers.Json.Converters;
 
 namespace Sirh3e.Steamer.Web.Models.SteamUser.FriendList
 {
     public class SteamerWebFriendModel
     {
         [JsonPropertyName("steamid")]
-        public string SteamId { get; set; }
+        [JsonConverter(typeof(SteamerSystemTextJsonConverterStringToUlong))]
+        public ulong SteamId { get; set; }
 
         [JsonPropertyName("relationship")]
         public string Relationship { get; set; }
 
         [JsonPropertyName("friend_since")]
-        public long FriendSince { get; set; }
+        public ulong FriendSince { get; set; }
     }
 }
