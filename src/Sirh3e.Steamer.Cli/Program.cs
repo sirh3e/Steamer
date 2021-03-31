04625dd9a452e4bc48eeca76481ab0cdca21597e
sirh3e <sirh3e@protonmail.com>
2021-03-31 16:24:07 +0200

    [REMOVED] src/Sirh3e.Steamer.Cli

--- src/Sirh3e.Steamer.Cli/Program.cs
+++ src/Sirh3e.Steamer.Cli/Program.cs
@@ -2,7 +2,9 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Sirh3e.Rust.Panic;
 using Sirh3e.Steamer.Web;
+using Sirh3e.Steamer.Web.Errors;
 using Sirh3e.Steamer.Web.Extensions.Interfaces.SteamUser.Request;
 using Sirh3e.Steamer.Web.Extensions.Rust;
 
@@ -12,30 +14,58 @@ namespace Sirh3e.Steamer.Cli
     {
         private static async Task Main(string[] args)
         {
-            var apiKey = "1A3E61134C39581437B9CFEADD8C87AD";
-            var (service, client) = SteamerWebFactory.CreateByKey(apiKey);
+            var key = "1A3E61134C39581437B9CFEADD8C87AD";
+            var (service, client) = SteamerWebFactory.CreateByKey(key);
 
             var stopwatch = Stopwatch.StartNew();
 
-            var steamId = await client
+            async Task<ulong> OnErr(ISteamerWebError error)
+            {
+                return (await error.HttpResponseMessage.Match(message =>
+                    {
+                        return (int)message.StatusCode switch
+                        {
+                            >= 400 and < 500 => client
+                                .SteamUser
+                                .ResolveVanityUrl
+                                .Build(error.Request.Method.Parameters)
+                                .ExecuteAsync(service),
+                            _ => throw new NotImplementedException()
+                        };
+                    }, () => throw new NotImplementedException()))
+                    .Map(response => response.Model)
+                    .Map(
+                        model =>
+                        model
+                            .Map(response => response.Response.SteamId)
+                            .UnwrapOr(0)
+                        )
+                    .UnwrapOr(0);
+            }
+
+            var result = await client
                 .SteamUser
                 .ResolveVanityUrl
                 .SetVanityUrl("xtrivax")
                 .Build()
-                .ExecuteAsync(service)
-                .MatchAsync(response => response.Model.Match(model => model.SteamId, () => throw new NotImplementedException()), error => throw new NotImplementedException());
+                .ExecuteAsync(service);
 
-            var steamIds = await client
+            var steamId = result
+                .MapOrElse(error => throw new PanicException(error.Message), response => response.Model.MapOr(0UL, model => model.Response.SteamId));
+
+            var task = await client
                 .SteamUser
                 .FriendList
                 .SetSteamId(steamId)
                 .Build()
-                .ExecuteAsync(service)
-                .MatchAsync(response => response.Model.Match(model =>
-                    model.FriendsList.Friends
-                    .OrderBy(friend => friend.FriendSince)
-                    .Select(friend => friend.SteamId)
-                    , () => throw new NotImplementedException()), err => throw new NotImplementedException());
+                .ExecuteAsync(service);
+
+            var steamIds = task.Map(
+                    response =>
+                    response.Model
+                        .Map(model => model.FriendsList.Friends.Select(friend => friend.SteamId))
+                        .UnwrapOrDefault())
+                .UnwrapOrDefault();
 
             stopwatch.Stop();
 
